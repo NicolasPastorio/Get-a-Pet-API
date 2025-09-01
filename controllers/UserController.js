8476e069b25f4048fc14f327e3dc2eb62f1265f9
NicolasPastorio <npastoriodemoura@gmail.com>
2025-08-31 22:39:42 -0300

    Iniciando validações de atualização de usuário.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,6 +5,7 @@ const User = require('../models/User');
 // helpers
 const createUserToken = require('../helpers/create-user-token');
 const getToken = require('../helpers/get-token');
+const getUserByToken = require('../helpers/get-user-by-token');
 
 module.exports = class UserController {
     static async register(req, res){
@@ -118,8 +119,52 @@ module.exports = class UserController {
         res.status(200).json({ user });
     }
 
-    static async aditUser(req, res){
-        res.status(200).json({ message: "Deu certo o udpdate!" });
-        return;
+    static async editUser(req, res){
+        const id = req.params.id;
+
+        // chek if user exists
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+        
+        const { name, email, phone, password, confirmpassword } = req.body;
+        
+        let image = '';
+        
+        // validatons
+        if(!name){
+            res.status(422).json({ message: 'O nome é obrigatório!' });
+            return;
+        }
+
+        if(!email){
+            res.status(422).json({ message: 'O email é obrigatório!' });
+            return;
+        }
+        const userExists = await User.findOne({ email: email });
+        if(user.email !== email && userExists){
+            res.status(422).json({ message: 'Por favor, utilize outro email!' });
+            return;
+        }
+        user.email = email;
+
+        if(!phone){
+            res.status(422).json({ message: 'O telefone é obrigatório' });
+            return;
+        }
+        if(!password){
+            res.status(422).json({ message: 'A senha é obrigatória!' });
+            return;
+        }
+        if(!confirmpassword){
+            res.status(422).json({ message: 'A confirmação de senha é obrigatória!' });
+            return;
+        }
+        if(password !== confirmpassword){
+            res.status(422).json({ message: 'A senha e a confirmação de senha precisam ser iguais!' });
+            return;
+        }
+        
+
+
     }
 }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -8,6 +8,6 @@ router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.get('/checkusers', UserController.checkUser);
 router.get('/:id', UserController.getUserById);
-router.patch('/edit/:id', verifyToken, UserController.aditUser);
+router.patch('/edit/:id', verifyToken, UserController.editUser);
 
 module.exports = router;
